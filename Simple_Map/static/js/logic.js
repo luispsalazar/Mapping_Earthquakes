38f39c90532189f8baf670c90e29758180234b81
Luis Salazar <luispsalazar@Luiss-iMac.local>
2022-04-18 19:24:38 -0500

    section 13.4.3 complete in branch Mapping_Lines

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -2,12 +2,26 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map("mapid", {
-    center: [
-      40.7, -94.5
-    ],
-    zoom: 4
-  });
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+//     center: [
+//       40.7, -94.5
+//     ],
+//     zoom: 4
+//   });
+
+// Coordinates for each point to be used in the line.
+// Coordinates for each point to be used in the polyline.
+let line = [
+  [33.9416, -118.4085],
+  [37.6213, -122.3790],
+  [40.7899, -111.9791],
+  [47.4502, -122.3088]
+];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+  color: "yellow"
+}).addTo(map);
 
 // Get data from cities.js
 let cityData = cities;
@@ -35,10 +49,10 @@ cityData.forEach(function(city) {
   // We create the tile layer that will be the background of our map.
 
 // for white background:
-// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}',{
+ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}',{
 
 // for dark background:
-  let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}',{
+//  let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}',{
 
 maxZoom: 18,
     accessToken: API_KEY
