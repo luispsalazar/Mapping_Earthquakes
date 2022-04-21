6106f037b38b38f544df509def4020a63dea0f8a
Luis Salazar <luispsalazar@Luiss-iMac.local>
2022-04-20 21:33:16 -0500

    added airports

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -10,11 +10,21 @@ console.log("working");
 //   });
 
 
+
 // Create the map object with center at the San Francisco airport.
 // let map = L.map('mapid').setView([37.5, -122.5], 10);
 
 
 // We create the tile layer that will be the background of our map.
+
+
+
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  accessToken: API_KEY
+});
 
 // for white background:
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}',{
@@ -23,11 +33,11 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
     accessToken: API_KEY
  });
 
-// We create the dark view tile layer that will be an option for our map.
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-  attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-  maxZoom: 18,
-  accessToken: API_KEY
+// Create the map object with center and zoom level.
+let map = L.map('mapid', {
+  center: [30, 30],
+  zoom: 2,
+  layers: [streets]
 });
 
 // Create a base layer that holds both maps.
@@ -39,12 +49,7 @@ let baseMaps = {
 // Then we add our 'graymap' tile layer to the map.
 dark.addTo(map);
 
-// Create the map object with center and zoom level.
-let map = L.map('mapid', {
-  center: [30, 30],
-  zoom: 2,
-  layers: [streets]
-});
+
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
@@ -52,7 +57,7 @@ L.control.layers(baseMaps).addTo(map);
 
 // Add GeoJSON data.
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/luispsalazar/Mapping_Earthquakes/main/Simple_Map/majorAirports.json";
+let airportData = "https://raw.githubusercontent.com/luispsalazar/Mapping_Earthquakes/main/majorAirports.json";
 
 
 // NOTE:
@@ -101,7 +106,7 @@ let airportData = "https://raw.githubusercontent.com/luispsalazar/Mapping_Earthq
 d3.json(airportData).then(function(data) {
   console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data).addTo(map);
+  L.geoJSON(data).bindPopup("<h2>" + features.properties.name + "</h2>").addTo(map)
 });
 
 
@@ -110,3 +115,5 @@ d3.json(airportData).then(function(data) {
 //     return L.marker(latlng);
 //   }
 // });
+
+//.bindPopup("<h2>" + properties.city + "</h2> <hr> <h3>Airport Name: " + properties.name + "</h3>")
