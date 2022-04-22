dcce05548cb53605494c98f636c4510ada76bc40
Luis Salazar <luispsalazar@Luiss-iMac.local>
2022-04-22 16:17:58 -0500

    updated Challenge

---
+++ Earthquake_Challenge/README.md
@@ -0,0 +1,65 @@
+# Mapping_Earthquakes
+
+Mapping Earthquakes creates a visualization to map earthquake data by latitude and longitude from the USGS. Data markers for each earthquake will show the magnitude as text and as a colored circle, where the size and color are larger and darker as the magnitude increases. Popups show additional information for each earthquake, like city, state, date and time. The color in the legend reflects the magnitude.
+
+
+## Deliverable 1
+
+The tectonic plate data is added as a second layer group.
+
+![11](Images/11.png)
+
+The tectonic plate data is added to the overlay object.
+
+![12](Images/12.png)
+
+The d3.json() callback is working and does the following:
+
+* The tectonic plate data is passed to the geoJSON() layer (file: tectonic_boundaries.json in "static" folder)
+* The geoJSON() layer adds color and width to the tectonic plate lines
+* The tectonic layer group variable is added to the map
+
+![13](Images/13.png)
+
+The earthquake data and tectonic plate data are displayed on the map when the page loads.
+
+![14](Images/14.png)
+
+
+## Deliverable 2
+
+The major earthquake data is added as a third layer group.
+
+![21](Images/21.png)
+
+The major earthquake data is added to the overlay object.
+
+![22](Images/22.png)
+
+The d3.json() callback is working and does the following:
+
+* Sets the color and diameter of each earthquake
+* The major earthquake data is passed to the geoJSON() layer
+* The geoJSON() layer creates a circle for each major earthquake, and adds a popup for each circle to display the magnitude and location of the earthquake
+* The major earthquake layer group variable is added to the map
+
+![23](Images/23.png)
+
+All the earthquake data and tectonic plate data are displayed on the map when the page loads and the datasets can be toggled on or off.
+
+![24](Images/24.png)
+
+
+## Deliverable 3
+
+A third map tile layer is created ("dark").
+
+![31](Images/31.png)
+
+The third map is added to the overlay object.
+
+![32](Images/32.png)
+
+All the earthquake data and tectonic plate data are displayed on the all maps of the webpage.
+
+![33](Images/33.png)
---
+++ Earthquake_Challenge/static/js/logicStep1.js
@@ -0,0 +1,132 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+// let map = L.map('mapid').setView([37.6213, -122.3790], 4);
+//     center: [
+//       40.7, -94.5
+//     ],
+//     zoom: 4
+//   });
+
+
+
+// Create the map object with center at the San Francisco airport.
+// let map = L.map('mapid').setView([37.5, -122.5], 10);
+
+
+// We create the tile layer that will be the background of our map.
+
+
+
+// We create the dark view tile layer that will be an option for our map.
+// let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//   attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+//   maxZoom: 18,
+//   accessToken: API_KEY
+// });
+
+// for white background:
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}',{
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+ });
+
+ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}',{
+  attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  Streets: streets,
+  Satellite: satelliteStreets
+};
+
+
+// Create the map object with center and zoom level.
+let map = L.map('mapid', {
+  center: [39.5, -98.5],
+  zoom: 3,
+  layers: [streets]
+});
+
+
+// Then we add our 'graymap' tile layer to the map.
+//satelliteStreets.addTo(map);
+
+
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+
+// Add GeoJSON data.
+// Accessing the airport GeoJSON URL
+// let airportData = "https://raw.githubusercontent.com/luispsalazar/Mapping_Earthquakes/main/majorAirports.json";
+
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
+});
+
+// NOTE:
+//  GeoJSON data coordinates are set with the first parameter
+//  as X (longitude) and the second parameter as Y (latitude)
+
+
+// Coordinates for each point to be used in the line.
+// Coordinates for each point to be used in the polyline.
+// let line = [
+//   [33.9416, -118.4085],
+//   [37.6213, -122.3790],
+//   [40.7899, -111.9791],
+//   [47.4502, -122.3088]
+// ];
+
+// Create a polyline using the line coordinates and make the line red.
+// L.polyline(line, {
+//   color: "yellow"
+// }).addTo(map);
+
+// Get data from cities.js
+// let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+// cityData.forEach(function(city) {
+//   console.log(city)
+//   L.circleMarker(city.location, {
+//     radius: city.population/150000,
+//     color: "orange"
+//   })
+//   .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//   .addTo(map);
+// });
+
+
+//  Add a circcle to the map for Los Angeles, California.
+// L.circleMarker(city.location, {
+//   radius: 30,
+//   color: "black",
+//   fillColor: '#ffffa1'
+// }).addTo(map);
+
+
+// // Grabbing our GeoJSON data.
+// d3.json(airportData).then(function(data) {
+//   console.log(data);
+//   // Creating a GeoJSON layer with the retrieved data.
+//   L.geoJSON(data).addTo(map)
+// });
+
+
+// L.geoJson(data, {
+//   pointToLayer: function(feature, latlng) {
+//     return L.marker(latlng);
+//   }
+// });
+
+//.bindPopup("<h2>" + properties.city + "</h2> <hr> <h3>Airport Name: " + properties.name + "</h3>")
